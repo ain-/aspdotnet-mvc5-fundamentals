a2e27b4cb6d303ef9dccbaaac51f217879d09831
Ain <ain.salula@gmail.com>
2017-01-16 05:25:01 +0200

    restricting users

---
+++ Identity/Identity/Controllers/SecretController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Identity.Controllers
+{
+    [Authorize]
+    public class SecretController : Controller
+    {
+        public ContentResult Secret()
+        {
+            return Content("This is a secret...");
+        }
+
+        [AllowAnonymous]
+        public ContentResult Overt()
+        {
+            return Content("This is not a secret...");
+        }
+    }
+}
