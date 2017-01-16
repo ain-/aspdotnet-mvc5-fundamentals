2899291eeed16f65d7df5fd206679914dbe0ad0c
Ain <ain.salula@gmail.com>
2017-01-16 03:10:28 +0200

    middleware pipeline with await next()

--- KatanaIntro/KatanaIntro/Program.cs
+++ KatanaIntro/KatanaIntro/Program.cs
@@ -29,6 +29,25 @@ namespace KatanaIntro
     {
         public void Configuration(IAppBuilder app)
         {
+            app.Use(async (environment, next) =>
+            {
+                foreach (var pair in environment.Environment)
+                {
+                    Console.WriteLine("{0}:{1}", pair.Key, pair.Value);
+                }
+
+                await next();
+            });
+
+            app.Use(async (environment, next) =>
+            {
+                Console.WriteLine("Requesting : " + environment.Request.Path);
+
+                await next();
+
+                Console.WriteLine("Response: " + environment.Response.StatusCode);
+            });
+
             app.UseHelloWorld();
         }
 
