c0951fb58aaa098070179b71ca8024c085e96353
Ain <ain.salula@gmail.com>
2017-01-17 14:18:42 +0200

    using UserManager to initialize a user

--- Identity/Identity/Migrations/Configuration.cs
+++ Identity/Identity/Migrations/Configuration.cs
@@ -1,6 +1,7 @@
 namespace Identity.Migrations
 {
     using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using Models;
     using System;
     using System.Data.Entity;
@@ -11,19 +12,27 @@ namespace Identity.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
             ContextKey = "Identity.Models.ApplicationDbContext";
         }
 
         protected override void Seed(Identity.Models.ApplicationDbContext context)
         {
-            var hasher = new PasswordHasher();
-            context.Users.AddOrUpdate(u => u.UserName,
-                new ApplicationUser
-                {
-                    UserName = "test",
-                    PasswordHash = hasher.HashPassword("password")
-                });
+            //var hasher = new PasswordHasher();
+            //context.Users.AddOrUpdate(u => u.UserName,
+            //    new ApplicationUser
+            //    {
+            //        UserName = "test",
+            //        PasswordHash = hasher.HashPassword("password")
+            //    });
+            if (!context.Users.Any(u => u.UserName == "test"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser { UserName = "test" };
+
+                manager.Create(user, "password");
+            }
         }
     }
 }
