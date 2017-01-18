c65d92bdc133a50d70e53f5bd34ffc89d87a7c71
Ain <ain.salula@gmail.com>
2017-01-18 11:44:18 +0200

    cross origin resource sharing

--- PatientData/PatientData/App_Start/WebApiConfig.cs
+++ PatientData/PatientData/App_Start/WebApiConfig.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
 
 namespace PatientData
 {
@@ -16,6 +17,9 @@ namespace PatientData
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            var cors = new EnableCorsAttribute("*", "*", "GET");
+            config.EnableCors();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
--- PatientData/PatientData/Controllers/PatientsController.cs
+++ PatientData/PatientData/Controllers/PatientsController.cs
@@ -7,9 +7,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace PatientData.Controllers
 {
+    [EnableCors("*", "*", "GET")]
     public class PatientsController : ApiController
     {
         IMongoCollection<Patient> _patients;
