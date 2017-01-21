57e69d2c186b21a31a85bacc3e15ef1850f6b4e9
Ain <ain.salula@gmail.com>
2017-01-21 12:30:37 +0200

    crud for books, creating a custom dropdown template

--- Books/Books.Entities/Genre.cs
+++ Books/Books.Entities/Genre.cs
@@ -1,10 +1,14 @@
-﻿namespace Books.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Books.Entities
 {
     public enum Genre
     {
+        [Display(Name="Non Fiction")]
         NonFinction,
         Romance,
         Action,
+        [Display(Name = "Science Fiction")]
         ScienceFiction
     }
 }
---
+++ Books/Books.Web/Helpers/EnumHelpers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Books.Web.Helpers
+{
+    public static class EnumHelpers
+    {
+        public static IEnumerable<SelectListItem> GetItems(this Type enumType, int? selectedValue)
+        {
+            if (!typeof(Enum).IsAssignableFrom(enumType))
+            {
+                throw new ArgumentException("Enum?");
+            }
+            var names = Enum.GetNames(enumType);
+            var values = Enum.GetValues(enumType).Cast<int>();
+
+            var items = names.Zip(values, (name, value) => 
+                new SelectListItem
+                {
+                    Text = GetName(enumType, name),
+                    Value = value.ToString(),
+                    Selected = value == selectedValue
+                }
+            );
+            return items;
+        }
+
+        static string GetName(Type enumType, string name)
+        {
+            var result = name;
+            var attribute = enumType.GetField(name)
+                .GetCustomAttributes(inherit: false)
+                .OfType<DisplayAttribute>()
+                .FirstOrDefault();
+            if (attribute != null)
+            {
+                result = attribute.GetName();
+            }
+            return result;
+        }
+    }
+}
