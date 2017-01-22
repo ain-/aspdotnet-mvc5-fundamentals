1364067879302ba896d2e70d6d492d7cacdd2fb2
Ain <ain.salula@gmail.com>
2017-01-22 23:05:11 +0200

    PerfServer

---
+++ PerfSurf/PerfSurf/Counters/PerfCounterWrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace PerfSurf.Counters
+{
+    public class PerfCounterWrapper
+    {
+        public PerfCounterWrapper(string name, string category, 
+            string counter, string instance = "")
+        {
+            _counter = new PerformanceCounter(category, counter, instance, readOnly: true);
+            Name = name;
+        }
+
+        public string Name { get; set; }
+        public float Value => _counter.NextValue();
+
+        PerformanceCounter _counter;
+    }
+}
--- PerfSurf/PerfSurf/Hubs/PerfHub.cs
+++ PerfSurf/PerfSurf/Hubs/PerfHub.cs
@@ -3,11 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+using PerfSurf.Counters;
 
 namespace PerfSurf.Hubs
 {
     public class PerfHub : Hub
     {
+        public PerfHub()
+        {
+            StartCounterCollection();
+        }
+
+        private void StartCounterCollection()
+        {
+            var task = Task.Factory.StartNew(async () =>
+            {
+                var perfService = new PerfCounterService();
+                while (true)
+                {
+                    var results = perfService.GetResults();
+                    Clients.All.newCounters(results);
+                    await Task.Delay(2000);
+                }
+            }, TaskCreationOptions.LongRunning);
+        }
+
         public void Send(string message)
         {
             Clients.All.newMessage(
